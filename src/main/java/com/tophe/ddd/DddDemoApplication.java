90497410535a60cc612228c9e1ebaeaf79f34dd6
Christophe Maldivi <christophe.maldivi@orange.com>
2017-10-31 20:21:22 +0100

    chore(import): from spring starter

---
+++ src/main/java/com/tophe/ddd/DddDemoApplication.java
@@ -0,0 +1,12 @@
+package com.tophe.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddDemoApplication.class, args);
+	}
+}
